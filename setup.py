67259848c5acbc647c0b778e29dd71f5a2c3fb6f
thecrazyfatguy <flaviokfiuza@gmail.com>
2021-04-06 15:18:24 -0300

    deleted LICENSE file, code from simple_interest_manager formatted and created empty setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup()
--- simple_interest_manager.py
+++ simple_interest_manager.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 
+
 def total_interest_calculator(periods, interest_rate):
 
-    total_interest = ((1+ interest_rate)**periods)
+    total_interest = (1 + interest_rate) ** periods
 
     return total_interest
+
 
 def final_amount_calculator(total_interest, init_amount):
 
@@ -13,20 +15,25 @@ def final_amount_calculator(total_interest, init_amount):
 
     return final_amount
 
+
 def net_income_calculator(final_amount, init_amount, tax_rate):
 
     net_before_taxes = final_amount - init_amount
 
-    total_taxes = net_before_taxes*tax_rate
+    total_taxes = net_before_taxes * tax_rate
 
     net_after_taxes = net_before_taxes - total_taxes
 
-    result = {'net_before_taxes':net_before_taxes, 'net_after_taxes':net_after_taxes, 'total_taxes':total_taxes}
+    result = {
+        "net_before_taxes": net_before_taxes,
+        "net_after_taxes": net_after_taxes,
+        "total_taxes": total_taxes,
+    }
 
     return result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     init_amount = 2000
 
     periods = 12
@@ -35,15 +42,23 @@ if __name__=="__main__":
 
     tax_rate = 0.225
 
-    total_interest = total_interest_calculator(periods = periods, interest_rate = interest_rate)
+    total_interest = total_interest_calculator(
+        periods=periods, interest_rate=interest_rate
+    )
 
-    print(f"The final interest rate during the whole period is: {(total_interest-1)*100}%")
+    print(
+        f"The final interest rate during the whole period is: {(total_interest-1)*100}%"
+    )
 
-    final_amount = final_amount_calculator(total_interest = total_interest, init_amount = init_amount)
+    final_amount = final_amount_calculator(
+        total_interest=total_interest, init_amount=init_amount
+    )
 
     print(f"The final calculated amount of the investment is: {final_amount}")
 
-    result = net_income_calculator(final_amount = final_amount, init_amount= init_amount, tax_rate=tax_rate)
+    result = net_income_calculator(
+        final_amount=final_amount, init_amount=init_amount, tax_rate=tax_rate
+    )
 
     print(f"The profit of the investment before taxes is: {result['net_before_taxes']}")
 
