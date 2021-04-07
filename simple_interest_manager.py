755f74dfa46d530bc37a0398cfddd4bbeda352ba
thecrazyfatguy <flaviokfiuza@gmail.com>
2021-04-07 17:20:51 -0300

    Changed input body text size and placement. Also, several little gramatical fixes in the dictionary.

--- simple_interest_manager.py
+++ simple_interest_manager.py
@@ -16,16 +16,16 @@ language_dictionary = {
     which will be increased by a fixed interest rate and taxes will be paid over the rent recieved. 
     Try it out!
     """,
-    'init_amount_body':"Insert here the initial investment amount",
+    'init_amount_body':""" ## Insert here the initial investment amount""",
     'init_amount_help':"Only positive values allowed",
     'init_amount_answ':'Initial amount inserted: US$ ',
-    'periods_body':"Insert here the number of periods you intend to mantain your investment",
+    'periods_body':""" ## Insert here the number of months you intend to mantain your investment""",
     'periods_help':"Only positive values allowed",
-    'periods_answ':'Number of periods inserted: ',
-    'interest_rate_body':"Insert here the interest rate (%) expected from your investment. This rate will be applied to your initial amount for each period of investment",
+    'periods_answ':'Number of months inserted: ',
+    'interest_rate_body':""" ## Insert here the monthly interest rate (%) expected from your investment.""",
     'interest_rate_help':"Only positive values. Only numbers. If your expected interest rate is 10%, enter the number 10",
     'interest_rate_answ':'Interest rate inserted: ',
-    'tax_rate_body':"Insert here the tax rate expected to be collected from your investment's revenue",
+    'tax_rate_body':""" ## Insert here the tax rate expected to be collected from your investment's revenue""",
     'tax_rate_help':"Only positive values allowed. Only numbers. If the expected tax rate is 20%, enter 20",
     'tax_rate_answ':"Tax rate inserted: ",
     'calculate_buttom':'Calculate',
@@ -36,7 +36,7 @@ language_dictionary = {
         ### At the end, your final amount will be:
         """,
     'net_before_taxes_title':""" 
-        ### Which means that your total gain (final - initial) is:
+        ### Which means that your total revenue is:
         """,
     'total_taxes_title':"""
         ### Then, you will have to pay the value below in the form of taxes
@@ -54,21 +54,21 @@ language_dictionary = {
         'app_intro': """
 
     Olá! Esta é uma calculadora de investimentos muito simples.
-    Eu desenvolvi este app como uma forma de praticar minhas habilidades de softwar (do back-end ao front-end).
+    Eu desenvolvi este app como uma forma de praticar minhas habilidades de software (do back-end ao front-end).
     Aqui voce pode realizar simulações simples de um único investimento ao longo de um limitado número de períodos,
     o valor deste investimento crescerá segundo uma taxa de juros fixa e impostos serão pagos sobre o rendimento recebido.
-    Experimente
+    Experimente!
     """,
-    'init_amount_body':"Insira aqui o valor do investimento inicial",
+    'init_amount_body':""" ## Insira aqui o valor do investimento inicial""",
     'init_amount_help':"Apenas valores positivos são permitidos",
-    'init_amount_answ':'Você inseriu R$ ',
-    'periods_body':"Insira aqui o número de períodos que você pretende manter o valor investido",
+    'init_amount_answ':'Montante inicial inserido: R$ ',
+    'periods_body':""" ## Insira aqui o número de meses que você pretende manter o valor investido""",
     'periods_help':"Apenas valores positivos são permitidos",
-    'periods_answ':'Total de períodos inseridos: ',
-    'interest_rate_body':"Insira aqui a taxa de juros (%) esperada neste investimento para cada período.",
+    'periods_answ':'Total de meses inseridos: ',
+    'interest_rate_body':""" ## Insira aqui a taxa de juros (%) esperada deste investimento para cada período.""",
     'interest_rate_help':"Apenas valores positivos. Apenas números. Se sua taxa de juros esperada for de 10%, insira o número 10",
     'interest_rate_answ':'Taxa de juros inserida: ',
-    'tax_rate_body':"Insira aqui a taxa do imposto a ser cobrado sobre o rendimento do investimento",
+    'tax_rate_body':""" ## Insira aqui a taxa do imposto a ser cobrado sobre o rendimento do investimento""",
     'tax_rate_help':"Apenas valores positivos são permitidos. Se o percentual de imposto esperado for de 20%, insira 20",
     'tax_rate_answ':"Taxa de imposto inserida: ",
     'calculate_buttom':'Calcular',
@@ -100,32 +100,36 @@ st.sidebar.write(
 st.title(language_dictionary[language]['app_title'])
 st.write(language_dictionary[language]['app_intro'])
 
+language_dictionary[language]['init_amount_body']
 init_amount = st.number_input(
-    language_dictionary[language]['init_amount_body'],
+    '',
     min_value=0.00,
     step=0.01,
     help=language_dictionary[language]['init_amount_help'],
 )
 st.write(language_dictionary[language]['init_amount_answ'], "{:,}".format(round(init_amount, 2)))
 
+language_dictionary[language]['periods_body']
 periods = st.number_input(
-    language_dictionary[language]['periods_body'],
+    '',
     min_value=0,
     step=1,
     help=language_dictionary[language]['periods_help'],
 )
 st.write(language_dictionary[language]['periods_answ'], "{:,}".format(periods))
 
+language_dictionary[language]['interest_rate_body']
 interest_rate = st.number_input(
-    language_dictionary[language]['interest_rate_body'],
+    '',
     min_value=0.0,
     step=0.1,
     help=language_dictionary[language]['interest_rate_help'],
 )
 st.write(language_dictionary[language]['interest_rate_answ'], "{:,}".format(round(interest_rate, 2)),"%")
 
+language_dictionary[language]['tax_rate_body']
 tax_rate = st.number_input(
-    language_dictionary[language]['tax_rate_body'],
+    '',
     min_value=0.0,
     step=0.1,
     help=language_dictionary[language]['tax_rate_help'],
